a86b675aca5a39347a29a8b582ebb163e10149d2
Erik Alfvin <erikalfvin@gmail.com>
2020-08-28 08:36:39 -0500

    React router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      MARS
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path="/" component={Home} />
+      </div>
+    </Router>
   );
 }
 
