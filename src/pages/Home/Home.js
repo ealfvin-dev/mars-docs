fe861737276ba504f76e6443cf4d68bf0eccbc11
Erik Alfvin <erikalfvin@gmail.com>
2020-08-29 07:58:44 -0500

    Header on each page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
-import Header from '../components/Header';
+import Header from '../../components/Header';
 
 import './Home.css'
 
@@ -10,10 +10,10 @@ function Home() {
             <Header subHeader="Mass Reduction Software"/>
             <div className="home">
                 <Grid container spacing={3}>
-                    <Grid item xs={6}>
+                    <Grid item xs={5}>
                         <h3>Find Errors in Input Files</h3>
                     </Grid>
-                    <Grid item xs={6}>
+                    <Grid item xs={7}>
                         <h3>Image...</h3>
                     </Grid>
                 </Grid>
