afae18f11cfbcc627067b395af55f23234ab28c8
Erik Alfvin <erikalfvin@gmail.com>
2020-08-30 23:08:20 -0500

    Added features summaries

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -25,14 +25,39 @@ function Home() {
                         <img src={require('./error-handling.jpg')} width='100%' alt="Input file with caught error" />
                     </Grid>
                     <Divider />
+                    <Grid item xs={4}>
+                        <h3>Perform for line-by-line air buoyancy corrections and center of mass gravitational corrections</h3>
+                        <p>
+                            
+                        </p>
+                    </Grid>
+                    <Grid item xs={5}>
+                        <img src={require('./ABC.jpg')} width='100%' alt="Input file with caught error" />
+                    </Grid>
+                    <Grid item xs={3}>
+                        <img src={require('./Gravity.jpg')} width='100%' alt="Input file with caught error" />
+                    </Grid>
+                    <Divider />
                     <Grid item xs={2}>
                         <img src={require('./split-series.jpg')} width='100%' alt="Split series menu view" />
                     </Grid>
-                    <Grid item xs={10}>
+                    <Grid item xs={5}>
                         <h3>View One Series at a Time</h3>
                         <p>
-                            Navigate between conected series in a calibration and view one series at a time.
+                            Navigate between connected series in a calibration and view one series at a time.
                         </p>
+                    </Grid>
+                    <Grid item xs={5}>
+                    </Grid>
+                    <Divider />
+                    <Grid item xs={5}>
+                        <h3>Keep Tack of Data That Has Been Entered in Each Series</h3>
+                        <p>
+                            Buttons colors will indicate what data has been entered in each series and what is left to complete.
+                        </p>
+                    </Grid>
+                    <Grid item xs={7}>
+                        <img src={require('./Input-Buttons.jpg')} width='70%' alt="Input file with caught error" />
                     </Grid>
                 </Grid>
             </div>
