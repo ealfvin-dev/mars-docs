0892d2a37ecea92c84301e211d720db08d222d2c
Erik Alfvin <erikalfvin@gmail.com>
2020-09-15 18:27:07 -0500

    Validation summary edits

--- src/pages/ValidationDetails/ValidationDetails.js
+++ src/pages/ValidationDetails/ValidationDetails.js
@@ -16,7 +16,7 @@ function ValidationDetails() {
                     accuracy of calculated results, acuracy of the output file and that results are not produced if bad data is input 
                     into the program. <br />  <br /> */}
                     This page provides details on validation that MARS has been through. All of the validation tests detailed 
-                    below are automatically re-run each time MARS is opened. This ensures that in the unlikely event of a file becoming 
+                    below have been checked manually and are also automatically re-run every time MARS is opened. This ensures that in the unlikely event of a file becoming 
                     corrupted or computer malfunction, an error will be displayed when the program is opened. These tests can also be run by clicking "Run Tests" 
                     in the menu. <br />
                     </p>
