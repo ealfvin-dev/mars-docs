8cd722bc44b946d1e3eabca089f0a1b2972b7264
Erik Alfvin <erikalfvin@gmail.com>
2020-09-20 11:46:54 -0500

    Home page logo

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,7 +7,25 @@ import './Home.css'
 function Home() {
     return (
         <div>
-            Home
+            {/* <Header subHeader="Mass Reduction Software"/> */}
+             <Grid container spacing={3}>
+                 <Grid item xs={12}>
+                    <p><br/></p>
+                 </Grid>
+                 <Grid item xs={12} style={{justifyContent: 'center'}}>
+                    <img src={require('../../components/logo-out.png')} height='200px' alt='Mars Logo' style={{display: 'block', margin: 'auto'}} />
+                 </Grid>
+                 <Grid item xs={12}>
+                    <h2 className='logoTitle' style={{textAlign: 'center'}}>Mass Reduction Software</h2>
+                </Grid>
+                <Grid item xs={4}>
+                </Grid>
+                <Grid item xs={4}>
+                    <h3 className='homepageText' style={{textAlign: 'center'}}>Powerful, user-friendly precision mass reduction software developed by the Technical Manager at the State of Minnesota Metrology Laboratory.</h3>
+                </Grid>
+                <Grid item xs={4}>
+                </Grid>
+            </Grid>
         </div>
     )
 }
