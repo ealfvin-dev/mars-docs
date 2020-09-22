d926a446e5227a7cedd3d0e325b25a113d7b8027
Erik Alfvin <erikalfvin@gmail.com>
2020-09-22 07:56:12 -0500

    4-1 view file

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import About from './pages/About/About';
 import Documentation from './pages/Documentation/Documentation';
 import ValidationDetails from './pages/ValidationDetails/ValidationDetails';
 import ExampleFiles from './pages/ExampleFiles/ExampleFiles';
+import ViewFile from './pages/ExampleFiles/ViewFile'
 import Features from './pages/Features/Features';
 
 const useStyles = makeStyles((theme) => ({
@@ -36,6 +37,7 @@ function App() {
           <Route exact path={"/documentation"} component={Documentation} />
           <Route exact path={"/validationdetails"} component={ValidationDetails} />
           <Route exact path={"/examplefiles"} component={ExampleFiles} />
+          <Route path={"/examplefile"} component={ViewFile} />
         </div>
       </div>
     </HashRouter>
---
+++ src/pages/ExampleFiles/ViewFile.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Header from '../../components/Header';
+import fileTexts from '../../exampleFiles/ExampleFileTexts'
+
+import './ExampleFiles.css'
+
+function ViewFile() {
+    const fileName = window.location.href.split('/').pop()
+    return (
+        <div>
+            <Header />
+            <div className='exampleFiles'>
+                <Grid container spacing={3}>
+                    <Grid item>
+                        {fileTexts[fileName] ? (
+                            <div>
+                                {fileName + '.txt'}
+                                <br/> <br/>
+                                <Paper elevation={0} className='filePaper'>
+                                    <div className='fileText'>
+                                        {fileTexts[fileName]}
+                                    </div>
+                                </Paper>
+                            </div>
+                        ) : null}
+                    </Grid>
+                </Grid>
+            </div>
+        </div>
+    )
+}
+
+export default ViewFile;
